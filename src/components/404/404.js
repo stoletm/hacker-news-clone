1c6f93c7d180a4fdeabb75957035248f4b59e80e
mstoletov <stoletov.police@gmail.com>
2023-06-09 21:39:21 +1000

    fix

--- src/components/404/404.js
+++ src/components/404/404.js
@@ -4,7 +4,7 @@ const Page404 = () => {
 
     return (
         <div style={{
-            fontSize: '40px', 
+            fontSize: '20px', 
             background: '#ddd', 
             width: '100%', 
             margin: '0 auto', 
@@ -14,14 +14,14 @@ const Page404 = () => {
         }}>
             <p>Oops, nothing here</p>
             <p style={{
-                fontSize: '70px', 
+                fontSize: '40px', 
                 margin: '20px'
             }}>
                 <b>(; - ;)</b>
             </p>
             <Link to='/' style={{
                 fontFamily:'Roboto', 
-                fontSize: '30px', 
+                fontSize: '18px', 
                 display: 'block', 
                 textDecoration: 'underline'
             }}>Back to main page</Link>
